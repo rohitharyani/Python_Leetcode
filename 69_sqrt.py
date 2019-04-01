e2ffe779d985bb95660cfbcabfc1b81e68ad26dd
Rohit Haryani <rohit.haryani196@gmail.com>
2019-03-31 19:28:32 -0700

    Add files via upload

---
+++ 69_sqrt.py
@@ -0,0 +1,18 @@
+'''
+Implement int sqrt(int x).
+
+Compute and return the square root of x, where x is guaranteed to be a non-negative integer.
+
+Since the return type is an integer, the decimal digits are truncated and only the integer part of the result is returned.
+'''
+class Solution:
+	def sqrt(self,x:int)->int:
+		return int(x**0.5)
+
+def main():
+	sol = Solution()
+	output = sol.sqrt(4)
+	print(output)
+
+if __name__=="__main__":
+	main()
